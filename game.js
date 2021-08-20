184723e8332469b2e9a1a6227c1666e50785cf27
phombos <68999764+phombos@users.noreply.github.com>
2021-08-20 08:08:51 +0700

    Update game.js

--- game.js
+++ game.js
@@ -1,14 +1,20 @@
 let canvas = document.createElement('canvas');
 document.body.appendChild(canvas);
 canvas.width = 800;
-canvas.height = 650;
+canvas.height = 600;
 let ctx = canvas.getContext('2d');
-let waitSpawn = 0.1;
+let waitSpawn = 0;
 let step = 0;
 let dt = 0.016;
+let keys = {
+	one: false,
+	two: false,
+	three: false,
+	four: false
+};
+let mouse = new Vec2(0, 0);
 
-
-let World = new world(new Vec2(0, 1), 400, 300, 1, 2);
+let World = new world(canvas.width, canvas.height, 1);
 
 window.requestAnimationFrame(function loop(){
 	clear();
@@ -17,16 +23,38 @@ window.requestAnimationFrame(function loop(){
 	World.update();
 	World.render();
 	step += dt;
-	if(step > waitSpawn){
-		let x = getRndInteger(99, 103);
-		let y = getRndInteger(0, 4);
-		World.addSand(x, y);
-		step = 0;
+	if(keys.one == true){
+		for(let i = mouse.x - 2; i < mouse.x + 3; i++){
+			for(let j = mouse.y - 2; j < mouse.y + 3; j++){
+				World.addSand(i, j);
+			}
+		}
+		
 	}
 	
 	window.requestAnimationFrame(loop);
 });
 
-window.addEventListener("keydown", function(code){
+window.addEventListener("mousemove", function(event){
+	let rect = canvas.getBoundingClientRect();
+	mouse.x = Math.round(event.clientX - rect.left);
+	mouse.y = Math.round(event.clientY - rect.top);
+  });
 
-}, false);
+window.addEventListener("keydown", function(key){
+	if(key.keyCode == "49"){
+		keys.one = true;
+	}
+	if(key.keyCode == "50"){
+		keys.two = true;
+	}
+});
+
+window.addEventListener("keyup", function(key){
+	if(key.keyCode == "49"){
+		keys.one = false;
+	}
+	if(key.keyCode == "50"){
+		keys.two = false;
+	}
+});
